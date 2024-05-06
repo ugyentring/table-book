7e1f88a9078273e0c13e675078d31c8dbba78910
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-07 04:26:11 +0600

    dashboard navigation done

--- client/src/booking/Home.jsx
+++ client/src/booking/Home.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useStore } from "react-redux";
+import { useSelector} from "react-redux";
 
 const Home = () => {
   const { user } = useSelector((state) => ({ ...state }));
--- client/src/user/Dashboard.jsx
+++ client/src/user/Dashboard.jsx
@@ -1,10 +1,14 @@
+import DashboardNav from "../components/DashboardNav";
+
 const Dashboard = () => {
   return (
     <>
       <div className="container-fluid bg-secondary p-5 text-center ">
         <h1>Dashboard</h1>
       </div>
-
+      <div className="container-fluid p-4">
+        <DashboardNav />
+      </div>
       <div className="container">
         <p>Show all booking and button to browse hotels</p>
       </div>
